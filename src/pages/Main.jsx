f17834b5708817415f5852dfbdf3a994e52c8c75
amangh30 <108615374+amangh30@users.noreply.github.com>
2024-01-28 13:24:01 +0530

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -34,6 +34,7 @@ const getFutureWeather= async(props)=>{
     }
     try{
         const res = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=b1d60fc2dd874b87894161306242601&q=${props.formData1},${props.formData2}&days=7`)
+        console.log(res)
         return res;
     }
     catch(e){
